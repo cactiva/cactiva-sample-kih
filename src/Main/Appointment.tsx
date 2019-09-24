8a9e1d6a4271dc8c096209f2a6ad23497e98da8e
IrulArif <masterirul@gmail.com>
2019-09-25 04:35:58 +0700

    fix

--- src/Main/Appointment.tsx
+++ src/Main/Appointment.tsx
@@ -1,11 +1,440 @@
-import { observer } from 'mobx-react-lite';
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { observer, useObservable } from "mobx-react-lite";
+import { View, TouchableOpacity } from "react-native";
+import { Text, List } from "react-native-ui-kitten";
+import { Icon } from "@src/libs";
+import { useNavigation } from "react-navigation-hooks";
 
 export default observer(() => {
+  const state = useObservable({
+    list: [
+      {
+        date: 24,
+        value: [
+          {
+            date: "Friday, 24 Sep 2019 at 09.30",
+            message: "dr. Frenanda",
+            submessage: "Poliklinik Umum I"
+          }
+        ]
+      },
+      {
+        date: 25,
+        value: null
+      },
+      {
+        date: 26,
+        value: [
+          {
+            date: "Friday, 24 Sep 2019 at 09.30",
+            message: "dr. Frenanda",
+            submessage: "Poliklinik Umum I"
+          }
+        ]
+      },
+      {
+        date: 27,
+        value: [
+          {
+            date: "Friday, 24 Sep 2019 at 09.30",
+            message: "dr. Frenanda",
+            submessage: "Poliklinik Umum I"
+          }
+        ]
+      },
+      {
+        date: 28,
+        value: null
+      },
+      {
+        date: 29,
+        value: null
+      },
+      {
+        date: 30,
+        value: [
+          {
+            date: "Friday, 24 Sep 2019 at 09.30",
+            message: "dr. Frenanda",
+            submessage: "Poliklinik Umum I"
+          }
+        ]
+      }
+    ]
+  });
+  const nav = useNavigation();
   return (
-    <View>
-      <Text>Halo</Text>
+    <View
+      style={{
+        marginTop: 15,
+        marginRight: 15,
+        marginBottom: 15,
+        marginLeft: 15,
+        flexGrow: 1
+      }}
+    >
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "stretch",
+          justifyContent: "center",
+          backgroundColor: "rgba(237, 247, 255, 0.58)",
+          borderRadius: 6
+        }}
+      >
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            backgroundColor: "#ffffff",
+            paddingLeft: 4,
+            paddingRight: 12,
+            paddingTop: 10,
+            paddingBottom: 10,
+            borderRadius: 6
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "rgba(237, 247, 255, 0)",
+              padding: 10,
+              borderRadius: 6
+            }}
+          >
+            <Text
+              style={{ color: "#37A2FF", fontSize: 22, fontFamily: "regular" }}
+              children={"September"}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "rgba(237, 247, 255, 0.58)",
+              padding: 10,
+              borderRadius: 2,
+              paddingTop: 8,
+              paddingRight: 8,
+              paddingBottom: 8,
+              paddingLeft: 8
+            }}
+            onPress={() => nav.navigate("Main/AppointmentCreate")}
+          >
+            <Icon
+              style={{ marginRight: 0, paddingRight: 0 }}
+              source={"AntDesign"}
+              name={"edit"}
+              size={20}
+              color={"#37A2FF"}
+            />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+            justifyContent: "space-between",
+            backgroundColor: "rgba(255, 255, 255, 0.37)",
+            paddingTop: 5,
+            paddingRight: 25,
+            paddingBottom: 5,
+            paddingLeft: 25
+          }}
+        >
+          <Text
+            style={{ backgroundColor: "", color: "#999999" }}
+            children={"S"}
+          />
+          <Text style={{ color: "#999999" }} children={"M"} />
+          <Text style={{ color: "#999999" }} children={"T"} />
+          <Text style={{ color: "#999999" }} children={"W"} />
+          <Text style={{ color: "#999999" }} children={"T"} />
+          <Text style={{ color: "#999999" }} children={"F"} />
+          <Text style={{ color: "#999999" }} children={"S"} />
+        </View>
+        <View
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "flex-start",
+            justifyContent: "space-between",
+            paddingLeft: 10,
+            paddingRight: 10,
+            paddingTop: 15,
+            paddingBottom: 15,
+            borderRadius: 6
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "rgba(55, 162, 255, 0)",
+              padding: 10,
+              borderRadius: 35
+            }}
+          >
+            <Text
+              style={{
+                paddingTop: 0,
+                paddingRight: 0,
+                paddingBottom: 0,
+                paddingLeft: 0,
+                backgroundColor: "",
+                borderRadius: 34,
+                color: ""
+              }}
+              children={"22"}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "rgba(255, 255, 255, 0)",
+              padding: 10,
+              borderRadius: 35
+            }}
+          >
+            <Text
+              style={{
+                paddingTop: 0,
+                paddingRight: 0,
+                paddingBottom: 0,
+                paddingLeft: 0,
+                backgroundColor: "",
+                borderRadius: 34
+              }}
+              children={"23"}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "rgba(55, 162, 255, 0.31)",
+              padding: 10,
+              borderRadius: 35
+            }}
+          >
+            <Text
+              style={{
+                paddingTop: 0,
+                paddingRight: 0,
+                paddingBottom: 0,
+                paddingLeft: 0,
+                backgroundColor: "",
+                borderRadius: 34
+              }}
+              children={"24"}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#ffffff",
+              padding: 10,
+              borderRadius: 35
+            }}
+          >
+            <Text
+              style={{
+                paddingTop: 0,
+                paddingRight: 0,
+                paddingBottom: 0,
+                paddingLeft: 0,
+                backgroundColor: "",
+                borderRadius: 34,
+                color: "#37A2FF"
+              }}
+              children={"25"}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "rgba(55, 162, 255, 0.31)",
+              padding: 10,
+              borderRadius: 35
+            }}
+          >
+            <Text
+              style={{
+                paddingTop: 0,
+                paddingRight: 0,
+                paddingBottom: 0,
+                paddingLeft: 0,
+                backgroundColor: "rgba(255, 255, 255, 0)",
+                borderRadius: 34
+              }}
+              children={"26"}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "rgba(55, 162, 255, 0.31)",
+              padding: 10,
+              borderRadius: 35
+            }}
+          >
+            <Text
+              style={{
+                paddingTop: 0,
+                paddingRight: 0,
+                paddingBottom: 0,
+                paddingLeft: 0,
+                backgroundColor: "",
+                borderRadius: 34
+              }}
+              children={"27"}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "rgba(55, 162, 255, 0)",
+              padding: 10,
+              borderRadius: 35
+            }}
+          >
+            <Text
+              style={{
+                paddingTop: 0,
+                paddingRight: 0,
+                paddingBottom: 0,
+                paddingLeft: 0,
+                backgroundColor: "",
+                borderRadius: 34
+              }}
+              children={"28"}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <List
+        data={state.list}
+        style={{
+          backgroundColor: "rgba(255, 255, 255, 0)",
+          marginTop: 15,
+          height: 0,
+          flexGrow: 1
+        }}
+        renderItem={({ item }: any) => {
+          return (
+            <>
+              <View
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "flex-start",
+                  justifyContent: "flex-start",
+                  height: 0,
+                  marginTop: 0,
+                  paddingLeft: 0,
+                  paddingRight: 0,
+                  paddingBottom: 0,
+                  flexGrow: 1,
+                  flexBasis: 0,
+                  minHeight: 95,
+                  paddingTop: null,
+                  marginBottom: 10
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 34,
+                    paddingTop: 5,
+                    paddingRight: 15,
+                    paddingLeft: 5,
+                    paddingBottom: 5,
+                    marginTop: 10,
+                    fontFamily: "regular",
+                    flexBasis: 49,
+                    color: "#a0a0a0"
+                  }}
+                  children={item.date}
+                />
+                {item.value && (
+                  <>
+                    <List
+                      style={{
+                        flexGrow: 1,
+                        flexBasis: 1,
+                        flexDirection: "column",
+                        alignItems: "stretch",
+                        justifyContent: "flex-start",
+                        backgroundColor: "rgba(255, 255, 255, 0)"
+                      }}
+                      data={item.value}
+                      renderItem={({ item }: any) => {
+                        return (
+                          <>
+                            <TouchableOpacity
+                              style={{
+                                display: "flex",
+                                flexDirection: "column",
+                                alignItems: "flex-start",
+                                justifyContent: "flex-start",
+                                backgroundColor: "rgba(244, 250, 255, 0.71)",
+                                padding: 10,
+                                borderRadius: 6,
+                                flexGrow: 1,
+                                marginBottom: 5
+                              }}
+                            >
+                              <Text
+                                style={{ fontFamily: "bold", fontSize: 16 }}
+                                children={item.date}
+                              />
+                              <Text
+                                style={{ fontSize: 14, fontFamily: "regular" }}
+                                children={item.message}
+                              />
+                              <Text
+                                style={{ fontSize: 14, fontFamily: "regular" }}
+                                children={item.submessage}
+                              />
+                            </TouchableOpacity>
+                          </>
+                        );
+                      }}
+                    />
+                  </>
+                )}
+              </View>
+            </>
+          );
+        }}
+      />
     </View>
   );
 });
--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,25 +1,27 @@
-import React from "react";
-import { observer } from "mobx-react-lite";
-import { View, TouchableOpacity } from "react-native";
-import { Text } from "react-native-ui-kitten";
-import { Icon } from "@src/libs";
+import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { View, TouchableOpacity } from 'react-native';
+import { Text } from 'react-native-ui-kitten';
+import { Icon } from '@src/libs';
+import { useNavigation } from 'react-navigation-hooks';
 
-export default observer(() => {
+export default observer(({ meta }: any) => {
+  const nav = useNavigation();
   return (
     <View
       style={{
-        flexDirection: "column",
-        alignItems: "stretch",
-        justifyContent: "flex-start",
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        justifyContent: 'flex-start',
         flexGrow: 1
       }}
     >
       <View
         style={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "flex-start",
-          justifyContent: "space-between",
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          justifyContent: 'space-between',
           marginTop: 42,
           marginBottom: 14,
           paddingLeft: 25
@@ -27,63 +29,63 @@ export default observer(() => {
       >
         <View
           style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            justifyContent: "flex-start"
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            justifyContent: 'flex-start'
           }}
         >
           <Text
             style={{
               fontSize: 22,
-              fontFamily: "regular",
+              fontFamily: 'regular',
               paddingLeft: 0,
-              color: "#73a6e0",
+              color: '#73a6e0',
               paddingBottom: 13
             }}
-            children={"Good Morning"}
+            children={'Good Morning'}
           />
           <Text
-            style={{ fontSize: 39, fontFamily: "light", color: "#4182c4" }}
-            children={"John Doe"}
+            style={{ fontSize: 39, fontFamily: 'light', color: '#4182c4' }}
+            children={'John Doe'}
           />
         </View>
         <TouchableOpacity
           style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "flex-start",
-            backgroundColor: "",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'flex-start',
+            backgroundColor: '',
             padding: 10,
             borderRadius: 6,
             marginTop: -10
           }}
         >
           <Icon
-            source={"EvilIcons"}
-            name={"user"}
+            source={'EvilIcons'}
+            name={'user'}
             size={40}
-            color={"#0062ff"}
+            color={'#0062ff'}
           />
           <Text
             style={{
-              fontFamily: "semibold",
+              fontFamily: 'semibold',
               fontSize: 16,
               paddingTop: 4,
-              color: "#0062ff"
+              color: '#0062ff'
             }}
-            children={"Profile"}
+            children={'Profile'}
           />
         </TouchableOpacity>
       </View>
       <TouchableOpacity
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "stretch",
-          justifyContent: "flex-start",
-          backgroundColor: "rgba(244, 250, 255, 0.71)",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'stretch',
+          justifyContent: 'flex-start',
+          backgroundColor: 'rgba(244, 250, 255, 0.71)',
           padding: 10,
           borderRadius: 6,
           marginTop: 13,
@@ -94,19 +96,19 @@ export default observer(() => {
       >
         <Text
           style={{
-            fontFamily: "semibold",
+            fontFamily: 'semibold',
             fontSize: 16,
-            color: "#37A2FF",
+            color: '#37A2FF',
             opacity: 0.7
           }}
-          children={"Medication reminders"}
+          children={'Medication reminders'}
         />
         <View
           style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            justifyContent: "flex-start",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            justifyContent: 'flex-start',
             marginTop: 14,
             marginBottom: 10,
             marginLeft: 28
@@ -114,68 +116,69 @@ export default observer(() => {
         >
           <Text
             style={{
-              fontFamily: "semibold",
+              fontFamily: 'semibold',
               fontSize: 23,
-              color: "#37A2FF",
+              color: '#37A2FF',
               paddingBottom: 17
             }}
-            children={"In the next 3 hours..."}
+            children={'In the next 3 hours...'}
           />
           <View
             style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-start",
-              justifyContent: "flex-start",
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
               marginTop: -5,
               marginBottom: 3,
               marginLeft: 15
             }}
           >
             <Text
-              style={{ fontFamily: "semibold", fontSize: 16, color: "#37A2FF" }}
-              children={"• Decolgen (1 tablet)"}
+              style={{ fontFamily: 'semibold', fontSize: 16, color: '#37A2FF' }}
+              children={'• Decolgen (1 tablet)'}
             />
             <Text
-              style={{ fontFamily: "semibold", fontSize: 16, color: "#37A2FF" }}
-              children={"• Amoxsan (1 tablet)"}
+              style={{ fontFamily: 'semibold', fontSize: 16, color: '#37A2FF' }}
+              children={'• Amoxsan (1 tablet)'}
             />
           </View>
         </View>
       </TouchableOpacity>
       <TouchableOpacity
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "stretch",
-          justifyContent: "flex-start",
-          backgroundColor: "rgba(255, 255, 255, 0.92)",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'stretch',
+          justifyContent: 'flex-start',
+          backgroundColor: 'rgba(255, 255, 255, 0.92)',
           padding: 10,
           borderRadius: 6,
           marginTop: 13,
           marginLeft: 20,
           marginRight: 20,
           marginBottom: 5,
-          borderStyle: "solid",
-          borderColor: "rgba(82, 188, 80, 0.69)",
+          borderStyle: 'solid',
+          borderColor: 'rgba(82, 188, 80, 0.69)',
           borderWidth: 3
         }}
+        onPress={() => (meta.page = 1)}
       >
         <Text
           style={{
-            fontFamily: "semibold",
+            fontFamily: 'semibold',
             fontSize: 16,
-            color: "#46cb43",
+            color: '#46cb43',
             opacity: 0.8
           }}
-          children={"Next Appointment"}
+          children={'Next Appointment'}
         />
         <View
           style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            justifyContent: "flex-start",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            justifyContent: 'flex-start',
             marginTop: 9,
             marginBottom: 10,
             marginLeft: 17
@@ -183,73 +186,73 @@ export default observer(() => {
         >
           <Text
             style={{
-              fontFamily: "bold",
+              fontFamily: 'bold',
               fontSize: 18,
-              color: "#52bc50",
+              color: '#52bc50',
               paddingBottom: 11
             }}
-            children={"Friday, 27 Sep 2019 at 09.30"}
+            children={'Friday, 26 Sep 2019 at 09.30'}
           />
           <View
             style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-start",
-              justifyContent: "flex-start",
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
               marginTop: 0,
               marginBottom: 3,
               marginLeft: 0
             }}
           >
             <Text
-              style={{ fontFamily: "semibold", fontSize: 16, color: "#46cb43" }}
-              children={"dr. ASTUTI, Sp.S  "}
+              style={{ fontFamily: 'semibold', fontSize: 16, color: '#46cb43' }}
+              children={'dr. ASTUTI, Sp.S  '}
             />
             <Text
               style={{
-                fontFamily: "semibold",
+                fontFamily: 'semibold',
                 fontSize: 16,
-                color: "#46cb43",
+                color: '#46cb43',
                 paddingTop: 2
               }}
-              children={"Poliklinik Umum I"}
+              children={'Poliklinik Umum I'}
             />
           </View>
         </View>
       </TouchableOpacity>
       <TouchableOpacity
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "stretch",
-          justifyContent: "flex-start",
-          backgroundColor: "rgba(255, 255, 255, 0.92)",
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'stretch',
+          justifyContent: 'flex-start',
+          backgroundColor: 'rgba(255, 255, 255, 0.92)',
           padding: 10,
           borderRadius: 6,
           marginTop: 13,
           marginLeft: 20,
           marginRight: 20,
           marginBottom: 5,
-          borderStyle: "solid",
-          borderColor: "rgba(240, 142, 77, 0.69)",
+          borderStyle: 'solid',
+          borderColor: 'rgba(240, 142, 77, 0.69)',
           borderWidth: 3
         }}
       >
         <Text
           style={{
-            fontFamily: "semibold",
+            fontFamily: 'semibold',
             fontSize: 16,
-            color: "#f08e4d",
+            color: '#f08e4d',
             opacity: 0.8
           }}
-          children={"Latest Lab Report"}
+          children={'Latest Lab Report'}
         />
         <View
           style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-start",
-            justifyContent: "flex-start",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'flex-start',
+            justifyContent: 'flex-start',
             marginTop: 9,
             marginBottom: 10,
             marginLeft: 17
@@ -257,36 +260,36 @@ export default observer(() => {
         >
           <Text
             style={{
-              fontFamily: "bold",
+              fontFamily: 'bold',
               fontSize: 18,
-              color: "#f08e4d",
+              color: '#f08e4d',
               paddingBottom: 10
             }}
-            children={"Chest Roentgen"}
+            children={'Chest Roentgen'}
           />
           <View
             style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "flex-start",
-              justifyContent: "flex-start",
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
               marginTop: 0,
               marginBottom: 3,
               marginLeft: 0
             }}
           >
             <Text
-              style={{ fontFamily: "semibold", fontSize: 16, color: "#f08e4d" }}
-              children={"22 Sep 2019 (two days ago)"}
+              style={{ fontFamily: 'semibold', fontSize: 16, color: '#f08e4d' }}
+              children={'22 Sep 2019 (two days ago)'}
             />
             <Text
               style={{
-                fontFamily: "semibold",
+                fontFamily: 'semibold',
                 fontSize: 16,
-                color: "#f08e4d",
+                color: '#f08e4d',
                 paddingTop: 2
               }}
-              children={"Status: Waiting Result"}
+              children={'Status: Waiting Result'}
             />
           </View>
         </View>
